8c23d2f854ccc910ed75c5cf710581fb30bfb73d
marekjanek <marekjanus3@wp.pl>
2020-06-11 16:39:51 +0200

    Fibonacci returning vector of int now

--- zadanie4.cpp
+++ zadanie4.cpp
@@ -9,21 +9,30 @@ int fibonacciRecursive(int n) {
     return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-int fibonnaciUsingAdjacent(int n) {
+std::vector<int> fibonnaciUsingAdjacent(int n) {
     std::vector<int> vec(n, 1);
     std::adjacent_difference(vec.begin(), vec.end() - 1, vec.begin() + 1, std::plus<int>());
-    return vec[n - 1];
+    return vec;
 }
 
-int fibonacciUsingTransform(int n) {
+std::vector<int> fibonacciUsingTransform(int n) {
     std::vector<int> vec(n, 1);
     std::transform(vec.begin(), vec.end() - 2, vec.begin() + 1, vec.begin() + 2, std::plus<>());  // [&sum](int& el) { sum += sum; return el=sum; });
-    return vec[n - 1];
+    return vec;
 }
 
 int main() {
-    std::cout << fibonacciRecursive(10) << '\n';
-    std::cout << fibonnaciUsingAdjacent(10) << '\n';
-    std::cout << fibonacciUsingTransform(10) << '\n';
+    std::cout << fibonacciRecursive(15) << '\n';
+    std::vector<int> one = fibonnaciUsingAdjacent(15);
+    std::vector<int> two = fibonacciUsingTransform(15);
+
+    for (auto el : one)
+        std::cout << el << " ";
+
+    std::cout << std::endl;
+
+    for (auto el : two)
+        std::cout << el << " ";
+
     return 0;
 }
