bb97b1555a73d7f28812d4b80d936a16274e40ba
malinaq3 <malinaq3@gmail.com>
2020-06-11 16:03:43 +0200

    almost working

--- zadanie7.cpp
+++ zadanie7.cpp
@@ -1,22 +1,27 @@
-#include<algorithm>
-#include<iostream>
-#include<iterator>
-#include<sstream>
-#include<vector>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <vector>
 
-auto lambdaPrint = [](auto element){std::cout << element << ' ';};
+void removeDupWord(std::string& str) {
+    std::size_t found = str.find_last_of('.');
 
-void removeDupWord(std::string str) { 
-  std::istringstream iss(str);
-  std::vector<std::string> results((std::istream_iterator<std::string>(iss)),std::istream_iterator<std::string>());
-  std::for_each(results.begin(), results.end(), lambdaPrint);
-  std::cout << '\n';
-  results.erase(std::unique(results.begin(), results.end()));
+    std::istringstream iss(str.substr(0, found));
+    std::vector<std::string> results((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
 
-  std::for_each(results.begin(), results.end(), lambdaPrint);
-} 
+    std::istringstream iss2(str.substr(found));
+    std::vector<std::string> results2((std::istream_iterator<std::string>(iss2)), std::istream_iterator<std::string>());
+    std::cout << '\n';
+    results.erase(std::unique(results.begin(), results.end()), results.end());
+    results.insert(results.end(), results2.begin(), results2.end());
+    str = std::accumulate(results.begin(), results.end(), std::string(), [](std::string lhs, const std::string& rhs) { return lhs.empty() ? rhs : lhs + ' ' + rhs; });
+}
 
-int main(){
-std::string str{"Ala ma ma kota, a kot ma ma Ale Ale."};
-removeDupWord(str);
+int main() {
+    std::string str{"Ala ma ma kota, a kot ma ma Ale Ale."};
+    std::cout << "Input: " << str << '\n';
+    removeDupWord(str);
+    std::cout << "Output: " << str << '\n';
 }
